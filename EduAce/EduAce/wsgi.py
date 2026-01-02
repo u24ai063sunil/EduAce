3a23f8bde7d808b61b9bb61cf63ab17d7923522d
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2026-01-02 21:16:13 +0530

    added

--- EduAce/EduAce/wsgi.py
+++ EduAce/EduAce/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduAce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduAce.EduAce.settings')
 
 application = get_wsgi_application()
